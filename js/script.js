28fc909dc81bc9e2da626918d738cef0dc80ba62
Karen <km4811@gmail.com>
2023-07-20 19:52:18 -0500

    made fuction that grabs quote API and puts in on HTML. Added elements to HTML to enhance layout

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //Adds event listener to document so that all HTML is loaded before JavaScript is applied
 document.addEventListener('DOMContentLoaded', () => {
-	const result = document.getElementById('results');
+	const gifResults = document.getElementById('gifResults');
+	const quoteResults = document.getElementById("quoteResults");
+
 	const form = document.getElementById('zenForm');
 
 	//Function that takes keyWord and uses it to fetch gif from API and add it to HTML
@@ -11,8 +13,22 @@ document.addEventListener('DOMContentLoaded', () => {
 		)
 			.then((res) => res.json())
 			.then((data) => {
-				console.log(data.data);
-				result.innerHTML = `<img src= ${data.data.images.original.url}>`;
+				//console.log(data.data);
+				gifResults.innerHTML = `<img src= ${data.data.images.original.url}>`;
+			});
+	}
+	//Function that takes keyWord and uses it to zenquote from API and add it to HTML
+	function quoteGenerator(keyWord) {
+		//Calls on zenquote API to get a random quotes with the tag of keyWord
+		//https://cors-proxy.fringe.zone/
+		fetch(
+			`https://zenquotes.io/api/random/2f2f146a00772400006c45f55e8e506e&keyword=${keyWord.toLowerCase()}`
+		)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				quoteResults.innerHTML = data[0].h;
+
 			});
 	}
 
@@ -20,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 		const keyWord = document.getElementById('categories').value;
+		quoteGenerator(keyWord);
 		gifGenerator(keyWord);
 	});
 });
